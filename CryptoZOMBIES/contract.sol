65ffac5cbde3556f37d5aa8eb2113d668ef32c31
namotoy <namotopet0615@gmail.com>
2023-03-24 22:27:07 +0900

    edit contract.sol

--- CryptoZOMBIES/contract.sol
+++ CryptoZOMBIES/contract.sol
@@ -1,6 +1,32 @@
 pragma solidity ^0.4.19;
 
 contract ZombieFactory {
-uint dnaDigits = 16;
-uint dnaModulus = 10 ** dnaDigits;
+// ゾンビを作成するとイベントが発生する
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+        }
+    
+    Zombie[] public zombies;
+
+  //  ゾンビを配列に追加する
+   function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna);
+    }
+// ゾンビのハッシュ値を計算する
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+// ランダムにゾンビを生成する
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 }
