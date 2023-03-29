d593c5690c5cf0d9a25628a80de50931da164dc2
namotoy <namotopet0615@gmail.com>
2023-03-29 22:42:52 +0900

    edit solidity version

--- CryptoZOMBIES/zombiefactory.sol
+++ CryptoZOMBIES/zombiefactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.19;
 
 contract ZombieFactory {
 // ゾンビを作成するとイベントが発生する
--- CryptoZOMBIES/zombiefeeding.sol
+++ CryptoZOMBIES/zombiefeeding.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.19;
 
 import "./zombiefactory.sol";
 // ZombieFactoryコントラクトを継承する
@@ -7,5 +7,8 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = zombies[_zombieId];
+      _targetDna = _targetDna % dnaModulus;
+      uint newDna = (myZombie.dna + _targetDna) / 2;
+      _createZombie("NoName", newDna);
   }
 }
