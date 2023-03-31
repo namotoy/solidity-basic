16f98a86b3b3cbfb89a91050d3e6104fb49166de
namotoy <namotopet0615@gmail.com>
2023-03-31 22:58:05 +0900

    update zombiefeeding.sol

--- CryptoZOMBIES/zombiefeeding.sol
+++ CryptoZOMBIES/zombiefeeding.sol
@@ -21,11 +21,23 @@ contract KittyInterface {
 // ZombieFactoryコントラクトを継承する
 // ゾンビが他の人間達を捕食すると、そのDNAが人間DNAと結合して新しいゾンビを生み出す
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = zombies[_zombieId];
       _targetDna = _targetDna % dnaModulus;
       uint newDna = (myZombie.dna + _targetDna) / 2;
+      if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
       _createZombie("NoName", newDna);
   }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
 }
